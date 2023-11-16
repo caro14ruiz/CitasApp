6d37de4fd883abc44ff30e5c13ea711492d4b340
caro14ruiz <carubu2001@gmail.com>
2023-11-16 07:58:31 -0600

    Sección9 - Commit for testing GitHub Desktop

--- SPA/src/app/_modules/shared.module.ts
+++ SPA/src/app/_modules/shared.module.ts
@@ -2,22 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
     ToastrModule.forRoot({
       timeOut: 10000,
       positionClass: 'toast-bottom-right',
       preventDuplicates: false,
       progressBar: true
-    }), 
+    }),
   ],
   exports: [
     BsDropdownModule,
-    ToastrModule
+    ToastrModule,
+    TabsModule
   ]
 })
 export class SharedModule { }
